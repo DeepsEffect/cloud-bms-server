ee5df4243d6207202ad203b892da46d336f4295c
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-06-06 16:36:50 +0600

    get the user info from db and send to client to define role

--- index.js
+++ index.js
@@ -69,6 +69,13 @@ async function run() {
       res.send(result);
     });
 
+    // get a user info from db
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await usersCollection.findOne({ email });
+      res.send(result);
+    });
+
     // get all the users data
     app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
